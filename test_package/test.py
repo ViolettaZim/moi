3aa983871729b571f0b54d740805c67db8c9df27
ViolettaZim <viozim@ya.ru>
2026-06-04 02:46:11 +0300

    Add gitignore and requirements file

---
+++ test_package/test.py
@@ -0,0 +1,9 @@
+def add(a: int, b: int) -> int:
+    """Add one in to another
+    Args:
+        a (int): num 1
+        b (int): num 2
+    Returns:
+        int: result a + b
+    """
+    return a + b
